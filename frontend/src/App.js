67431501526ad0c25286435e81042962fb76d763
ramiawed-eng <ramiawed-eng@gmail.com>
2020-10-28 13:45:03 +0300

    finish starting the Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
-  return <h1>Welcome To ProShop</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route exact path='/' component={HomeScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 export default App;
